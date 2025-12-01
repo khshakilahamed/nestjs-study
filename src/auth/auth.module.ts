f244c319e685a2e9881d71c41bf4f94e776fdf6b
khshakilahamed <khshakil.ahamed18@gmail.com>
2025-12-01 18:34:57 +0600

    incomplete refresh token 17

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,12 +9,14 @@ import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from './config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import refreshJwtConfig from './config/refresh-jwt.config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.registerAsync(jwtConfig.asProvider()),
-    ConfigModule.forFeature(jwtConfig)
+    ConfigModule.forFeature(jwtConfig),
+    ConfigModule.forFeature(refreshJwtConfig),
   ],
   controllers: [AuthController],
   providers: [AuthService, UserService, LocalStrategy, JwtStrategy],
