c5fcaefa895ade5e02547569d68146a512804350
khshakilahamed <khshakil.ahamed18@gmail.com>
2025-12-02 20:44:08 +0600

    middlware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
@@ -12,10 +12,15 @@ import { CustomerModule } from './customer/customer.module';
 import { MynameController } from './myname/myname.controller';
 import { UserRolesController } from './user-roles/user-roles.controller';
 import { ExceptionController } from './exception/exception.controller';
+import { LoggerMiddleware } from './middleware/logger/logger.middleware';
 
 @Module({
   imports: [EmployeeModule, CategoryModule, StudentModule, CustomerModule],
   controllers: [AppController, UserController, ProductController, MynameController, UserRolesController, ExceptionController],
   providers: [AppService, UserService, ProductService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/exception/exception.controller.ts
+++ src/exception/exception.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe, UseFilters } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, ParseIntPipe, UseFilters } from '@nestjs/common';
 import { HttpExceptionFilter } from 'src/filters/http-exception/http-exception.filter';
 
 @Controller('exception')
@@ -6,6 +6,10 @@ import { HttpExceptionFilter } from 'src/filters/http-exception/http-exception.f
 export class ExceptionController {
       @Get('/hello/:id')
       getHello(@Param('id', ParseIntPipe) id: number) {
+
+            /* if(id === 1){
+                  throw new BadRequestException("One is not valid id.")
+            } */
             return { Message: `Your Id Is: ${id}` };
       }
 }
