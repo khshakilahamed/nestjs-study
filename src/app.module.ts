a7e1699e19961f83dcd6a39aaa4f15fb2249aee6
khshakilahamed <khshakil.ahamed18@gmail.com>
2025-12-02 21:15:39 +0600

    lifecycle event

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,11 +13,14 @@ import { MynameController } from './myname/myname.controller';
 import { UserRolesController } from './user-roles/user-roles.controller';
 import { ExceptionController } from './exception/exception.controller';
 import { LoggerMiddleware } from './middleware/logger/logger.middleware';
+import { DatabaseService } from './database/database.service';
+import { DatabaseController } from './database/database.controller';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [EmployeeModule, CategoryModule, StudentModule, CustomerModule],
-  controllers: [AppController, UserController, ProductController, MynameController, UserRolesController, ExceptionController],
-  providers: [AppService, UserService, ProductService],
+  imports: [EmployeeModule, CategoryModule, StudentModule, CustomerModule, DatabaseModule],
+  controllers: [AppController, UserController, ProductController, MynameController, UserRolesController, ExceptionController, DatabaseController],
+  providers: [AppService, UserService, ProductService, DatabaseService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
