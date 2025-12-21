48d39e11ed27ebb7f2c33da4127269ab97db147e
khshakilahamed <khshakil.ahamed18@gmail.com>
2025-12-22 01:10:59 +0600

    typeorm integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,40 +1,22 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { EmployeeModule } from './employee/employee.module';
-import { CategoryModule } from './category/category.module';
-import { MynameController } from './myname/myname.controller';
-import { UserRolesController } from './user-roles/user-roles.controller';
-import { ExceptionController } from './exception/exception.controller';
 import { LoggerMiddleware } from './middleware/logger/logger.middleware';
-import { DatabaseService } from './database/database.service';
-import { DatabaseController } from './database/database.controller';
-import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { StudentModule } from './student/student.module';
 import { UserModule } from './user/user.module';
-import { ProductModule } from './product/product.module';
-import { LibraryModule } from './library/library.module';
-import { ProjectModule } from './project/project.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    EmployeeModule,
-    CategoryModule,
-    DatabaseModule,
-    ConfigModule.forRoot({
-      isGlobal: true
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.SUPABASE_DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
-    MongooseModule.forRoot(process.env.MONGO_URI!),
-    StudentModule,
-    UserModule,
-    ProductModule,
-    LibraryModule,
-    ProjectModule,
+    UserModule
   ],
-  controllers: [AppController, MynameController, UserRolesController, ExceptionController, DatabaseController],
-  providers: [AppService, DatabaseService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './schemas/user.schema';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
-      imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
-      controllers: [UserController],
-      providers: [UserService]
+      imports: [ TypeOrmModule.forFeature([User])],
 })
-export class UserModule { }
+export class UserModule {}
