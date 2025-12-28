9fddb831051259d549922d3f79dde30a1cac8ae5
khshakilahamed <khshakil.ahamed18@gmail.com>
2025-12-28 21:29:18 +0600

    get data

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { Employee } from './employees.entity';
 import { EmployeesService } from './employees.service';
 
@@ -10,4 +10,15 @@ export class EmployeesController {
       async createEmployee(@Body() body: Partial<Employee>): Promise<Employee>{
             return this.employeeService.create(body);
       }
+
+      @Get()
+      async findAll(): Promise<Employee[]>{
+            return this.employeeService.findAll();
+      }
+
+      @Get(':id')
+      async findOne(@Param('id') id: number): Promise<Employee>{
+            return this.employeeService.findOne(id);
+      }
+      
 }
--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Employee } from './employees.entity';
 import { Repository } from 'typeorm';
@@ -7,12 +7,27 @@ import { Repository } from 'typeorm';
 export class EmployeesService {
       constructor(
             @InjectRepository(Employee) private employeeRepository: Repository<Employee>
-      ){}
+      ) { }
 
-      async create(employeeData: Partial<Employee>): Promise<Employee>{
+      async create(employeeData: Partial<Employee>): Promise<Employee> {
             const employee = this.employeeRepository.create(employeeData);
 
             return this.employeeRepository.save(employee);
 
+      }
+
+
+      async findAll(): Promise<Employee[]> {
+            return this.employeeRepository.find();
+      }
+
+      async findOne(id: number): Promise<Employee>{
+            const employee = await this.employeeRepository.findOneBy({id});
+
+            if(!employee){
+                  throw new NotFoundException(`Employee with ID ${id} not found.`)
+            }
+
+            return employee;
       }
 }
